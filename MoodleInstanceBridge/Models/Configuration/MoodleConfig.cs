7cf129faccf2f3fbf8f69af31cfeb9fef5117e6d
Binon <binon>
2026-01-13 15:01:40 +0000

    Added ref to Model project and inmported 2 functions

---
+++ MoodleInstanceBridge/Models/Configuration/MoodleConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace MoodleInstanceBridge.Models.Configuration
+{
+    /// <summary>
+    /// The Moodle Settings.
+    /// </summary>
+    public class MoodleConfig
+    {
+        /// <summary>
+        /// Gets or sets the base url for the Moodle service.
+        /// </summary>
+        public string ApiBaseUrl { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the Web service Rest Format.
+        /// </summary>
+        public string ApiWsRestFormat { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the token.
+        /// </summary>
+        public string ApiWsToken { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the token.
+        /// </summary>
+        public string ApiPath { get; set; } = "webservice/rest/server.php";
+
+        /// <summary>
+        /// Gets or sets the token.
+        /// </summary>
+        public string CoursePath { get; set; } = "course/view.php";
+    }
+}
---
+++ MoodleInstanceBridge/Services/HttpClients/MoodleHttpClient.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Options;
+using MoodleInstanceBridge.Interfaces;
+using MoodleInstanceBridge.Models.Configuration;
+using System.Net.Http.Headers;
+
+namespace MoodleInstanceBridge.Services.HttpClients
+{
+
+    /// <summary>
+    /// The Moodle Http Client.
+    /// </summary>
+    public class MoodleHttpClient : IMoodleHttpClient, IDisposable
+    {
+        private readonly MoodleConfig moodleConfig;
+        private readonly HttpClient httpClient = new();
+
+        private bool initialised = false;
+        private string moodleApiUrl;
+        private string moodleApiMoodleWsRestFormat;
+        private string moodleApiWsToken;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoodleHttpClient"/> class.
+        /// </summary>
+        /// <param name="httpClient">httpClient.</param>
+        /// <param name="moodleConfig">config.</param>
+        public MoodleHttpClient(HttpClient httpClient, IOptions<MoodleConfig> moodleConfig)
+        {
+            this.moodleConfig = moodleConfig.Value;
+            this.httpClient = httpClient;
+
+            this.moodleApiUrl = this.moodleConfig.ApiBaseUrl + this.moodleConfig.ApiPath;
+            this.moodleApiMoodleWsRestFormat = this.moodleConfig.ApiWsRestFormat;
+            this.moodleApiWsToken = this.moodleConfig.ApiWsToken;
+        }
+
+        /// <summary>
+        /// The Get Client method.
+        /// </summary>
+        /// <returns>The <see cref="Task"/>.</returns>
+        public async Task<HttpClient> GetClient()
+        {
+            this.Initialise(this.moodleApiUrl);
+            return this.httpClient;
+        }
+
+        /// <summary>
+        /// GetDefaultParameters.
+        /// </summary>
+        /// <returns>defaultParameters.</returns>
+        public string GetDefaultParameters()
+        {
+            string defaultParameters = $"wstoken={this.moodleApiWsToken}"
+                              + $"&moodlewsrestformat={this.moodleApiMoodleWsRestFormat}";
+
+            return defaultParameters;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// The dispoase.
+        /// </summary>
+        /// <param name="disposing">disposing.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.httpClient.Dispose();
+            }
+        }
+
+        private void Initialise(string httpClientUrl)
+        {
+            if (this.initialised == false)
+            {
+                this.httpClient.BaseAddress = new Uri(httpClientUrl);
+                this.httpClient.DefaultRequestHeaders.Accept.Clear();
+                this.httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                this.initialised = true;
+            }
+        }
+    }
+}
